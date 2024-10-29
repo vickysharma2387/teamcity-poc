cbb7c1f6b750c2d03a7d183f25c49819a38506c7
Vicky Sharma <vicky_sharma@persistent.com>
2024-10-29 18:30:30 +0530

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,14 @@ resource "aws_ecs_cluster" "ecs_cluster" {
   name = "ecs-cluster"
 }
 
+data "aws_ssm_parameter" "ecs_ami" {
+name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+}
+
+
 resource "aws_launch_configuration" "ecs_launch_config" {
   name          = "ecs-launch-config"
-  image_id      = "ami-0c55b159cbfafe1f0"
+  image_id      = data.aws_ssm_parameter.ecs_ami.value
   instance_type = "t2.micro"
 
   lifecycle {
