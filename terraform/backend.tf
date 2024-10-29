23082010567af0f748cf96701b1077233b99d024
Vicky Sharma <vicky_sharma@persistent.com>
2024-10-29 17:29:55 +0530

    Updated backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-backend-teamcity"
-    key            = "s3://terraform-backend-teamcity/terraform/"
+    key            = "terraform/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "DT-terraform-backend-teamcity"
     encrypt        = true
